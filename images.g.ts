12e2178a39406ac991b688840df751402493c7a5
Rylanchad <ldepenhart31@gmail.com>
2025-04-03 08:50:42 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,63 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . 3 . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "myImage0":return img`
+d d d d d d d d d . . . . . . . 
+2 2 2 2 2 2 d 2 d . . . . . . . 
+d 2 d 2 2 2 2 2 d . . . . . . . 
+d 2 2 2 2 d 2 2 d . . . . . . . 
+d 2 2 2 2 2 2 2 d . . . . . . . 
+2 2 f 2 2 2 f 2 2 . . . . . . . 
+d 2 2 2 2 2 2 2 d . . . . . . . 
+d d 2 2 d 2 2 2 d . . . . . . . 
+d 2 2 2 2 2 2 d d . . . . . . . 
+2 2 2 f f f 2 2 d . . . . . . . 
+d 2 f 2 2 2 f 2 2 . . . . . . . 
+d 2 2 2 d 2 2 2 d . . . . . . . 
+2 d 2 2 2 2 2 d d . . . . . . . 
+d 2 2 2 d 2 2 2 d . . . . . . . 
+d 2 2 d 2 2 2 d d . . . . . . . 
+d d 2 d d 2 d d 2 . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
